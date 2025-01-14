c48fcd2e62e3436d4ca42ccbe5573c43bc6aa060
Abdulmajid <ermatovabdumajid23@gmail.com>
2025-01-14 21:17:41 +0500

    Implement header component with navigation and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+    </>
+  );
 };
 
 export default App;
