94f7ed8d15b978eb2503529bb95041e2ee15e982
Abdulmajid <ermatovabdumajid23@gmail.com>
2025-01-14 21:26:07 +0500

    The benefints section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
     </>
   );
