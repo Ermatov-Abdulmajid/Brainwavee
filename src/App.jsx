1a81692d387e45dcdab06da8754a95bf54098148
Abdulmajid <ermatovabdumajid23@gmail.com>
2025-01-14 21:44:53 +0500

    The site is fully comlemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
-import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -17,7 +19,10 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
+
+      <ButtonGradient />
     </>
   );
 };
